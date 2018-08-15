1bb4aa025f905ce08a744f9963fb71d31b3c51f2
Mary Jackson <jackson.marytherese@gmail.com>
2018-08-15 02:03:37 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,13 @@
+class FiguresController < ApplicationController
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    
+  end
+
+end
