464ed58a792ef3dfe5b2c990f32dcdb62d2a0954
Mary Jackson <jackson.marytherese@gmail.com>
2018-08-17 12:02:00 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,44 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    @figures = Figure.all
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    if !params["figure"]["name"].empty?
+      @landmark.figures << Figure.create(name: params["figure"]["name"])
+    end
+
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params["landmark"])
+    if !params["figure"]["name"].empty?
+      @landmark.figures << Figure.create(name: params["figure"]["name"])
+    end
+    
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
 end
