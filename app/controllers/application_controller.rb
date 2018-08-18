4b96700053dd4e83517abce72b1c30d00ff1654a
Mary Jackson <jackson.marytherese@gmail.com>
2018-08-18 12:16:13 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
 
-  use Rack::MethodOverride
-
   get '/' do
     erb :'application/root'
   end
